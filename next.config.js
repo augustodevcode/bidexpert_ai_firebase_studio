c391e40316435025659512df18bd5c525fa42a84
Augusto Araujo <augustoaraujo.augusto@gmail.com>
2025-09-06 18:58:03 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ next.config.js
@@ -0,0 +1,33 @@
+/** @type {import('next').NextConfig} */
+
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const config = {
+  /* config options here */
+  typescript: {
+    // Em desenvolvimento, ignoramos erros para não quebrar o HMR. Em produção, forçamos a verificação.
+    ignoreBuildErrors: isDevelopment,
+  },
+  eslint: {
+    // Em desenvolvimento, ignoramos erros para não quebrar o HMR. Em produção, forçamos a verificação.
+    ignoreDuringBuilds: isDevelopment,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = config;
