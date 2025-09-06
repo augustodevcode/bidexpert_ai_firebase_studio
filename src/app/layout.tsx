a5747ece73edc2bb617827047aa8b33a7ddf5d2c
Augusto Araujo <augustoaraujo.augusto@gmail.com>
2025-09-06 19:33:12 +0000

    o arquivo sample data .json foi excluído e devemos usar somente o banco

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-
 import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { AuthProvider } from '@/contexts/auth-context';
 import { AppContentWrapper } from './app-content-wrapper';
+
+console.log('[layout.tsx] LOG: RootLayout component is rendering/executing.');
 
 export const metadata: Metadata = {
   title: 'BidExpert - Leilões Online',
@@ -21,10 +22,10 @@ export default function RootLayout({
     <html lang="pt-BR" suppressHydrationWarning>
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
       </head>
-      <body className="antialiased bg-background text-foreground">
+      <body className="font-body antialiased bg-background text-foreground">
         <AuthProvider>
           <TooltipProvider delayDuration={0}>
             <AppContentWrapper>{children}</AppContentWrapper>
@@ -35,3 +36,4 @@ export default function RootLayout({
     </html>
   );
 }
+
