62ec048044612ac0aeb4982cbf9d7e5a7c0280af
Augusto Araujo <augustoaraujo.augusto@gmail.com>
2025-09-06 18:12:36 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-console.log("[next.config.mjs] LOG: Reading Next.js configuration.");
+console.log(`[next.config.mjs] LOG: Reading Next.js configuration for NODE_ENV: ${process.env.NODE_ENV}`);
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
