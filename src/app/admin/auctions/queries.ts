d0db91acbeffa263298143d3bc2b5c0673b006db
Augusto Araujo <augustoaraujo.augusto@gmail.com>
2025-10-04 03:59:51 +0000

    Funcionalidade: Ok, continue implementando o backlog
    UI:
    o className

--- src/app/admin/auctions/queries.ts
+++
@@ -1,2 +0,0 @@
-// This file is now obsolete and will be deleted.
-// All database queries are now done through Prisma in server actions.
--- src/types/index.js
+++
@@ -1,4 +0,0 @@
-// This file is intentionally left blank. It is used to resolve module imports in some environments.
-// All type definitions are in index.ts
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
